9a9221453d198faa4866ea68931a70681b8f4b2f
jadsonmarliere <jds.mdo@gmail.com>
2019-09-05 12:59:50 -0300

    Update ServerSide.js

--- ServerSide.js
+++ ServerSide.js
@@ -1,7 +1,16 @@
-var http = require('http')
+// exec> node ServerSide.js
 
-// escuta porta 8080
+//create my own module
+exports.myDateTime = function(){
+	return Date();
+	
+};
+
+var http = require('http'); // name of the module/library
+var dt = require('./myModule');
+
 http.createServer(function(req, res){
 	res.writeHead(300, {'Content-Type': 'text/html'})
+	res.write("A data e o horários agora são: " + dt.myDateTime());
 	res.end('Teste Server Node JS ')
-}).listen(8080);
+}).listen(8080); // hear port 8080
