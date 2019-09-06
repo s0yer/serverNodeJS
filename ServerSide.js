f70e00ae8e7d6daae9727e3fd817150ba43d25df
jadsonmarliere <jds.mdo@gmail.com>
2019-09-06 19:37:15 -0300

    Update ServerSide.js

--- ServerSide.js
+++ ServerSide.js
@@ -1,11 +1,22 @@
+// by Jadson Marliere de Oliveira
 // exec> node ServerSide.js
 
 
 var http = require('http'); // name of the module/library
 var dt = require('./myModule'); // include a module file
+var url = require('url')
 
+//create a server object
 http.createServer(function(req, res){
-	res.writeHead(300, {'Content-Type': 'text/html'})
+	res.writeHead(200, {'Content-Type': 'text/html'})// return a response to the client , 200 -> all is OK, 2argument-> response headers
+	
 	res.write("A data e o horario hoje: " + dt.myDateTime()); // module function
-	res.end('Teste Server Node JS ')
-}).listen(8080); // hear port 8080
+	
+	var q = url.parse(req.url, true).query;
+	var txt = q.year + " " + q.month;
+	
+	res.write(req.url)
+	
+	res.end(txt)
+	res.end('Teste Server Node JS ') //end the response
+}).listen(8080); // listen port 8080
