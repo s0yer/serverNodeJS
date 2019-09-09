737782c07afdb97718b3bd44bf70443ddf3525ae
jadsonmarliere <jds.mdo@gmail.com>
2019-09-09 15:49:59 -0300

    create File System module

--- ServerSide.js
+++ ServerSide.js
@@ -4,19 +4,24 @@
 
 var http = require('http'); // name of the module/library
 var dt = require('./myModule'); // include a module file
-var url = require('url')
+var url = require('url');
+var fs = require('fs'); // File System Module
+
 
 //create a server object
 http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/html'})// return a response to the client , 200 -> all is OK, 2argument-> response headers
+	fs.readFile('demoFile.html', function(err, data){ // read files -> html files
 	
-	res.write("A data e o horario hoje: " + dt.myDateTime()); // module function
-	
-	var q = url.parse(req.url, true).query;
-	var txt = q.year + " " + q.month;
-	
-	res.write(req.url)
-	
-	res.end(txt)
-	res.end('Teste Server Node JS ') //end the response
+		res.writeHead(200, {'Content-Type': 'text/html'});// return a response to the client , 200 -> all is OK, 2argument-> response headers
+		
+		res.write("A data e o horario hoje: " + dt.myDateTime()); // module function
+		
+		var q = url.parse(req.url, true).query;
+		var txt = q.year + " " + q.month;
+		
+		res.write(req.url);
+		
+		res.end(txt);
+		res.end('Teste Server Node JS '); //end the response
+	});
 }).listen(8080); // listen port 8080
