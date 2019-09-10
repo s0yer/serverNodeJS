bf2f758a1714ba5d602c2004e90cc7369003cecf
jadsonmarliere <jds.mdo@gmail.com>
2019-09-10 13:59:48 -0300

    Update ServerSide.js

--- ServerSide.js
+++ ServerSide.js
@@ -4,8 +4,11 @@
 
 var http = require('http'); // name of the module/library
 var dt = require('./myModule'); // include a module file
-var url = require('url');
+var url = require('url'); // include a URL module
 var fs = require('fs'); // File System Module
+
+var adr = 'http://localhost:8080/default.htm?year=2019&month=september';
+var q = url.parse(adr, true);
 
 
 //create a server object
@@ -13,15 +16,23 @@ http.createServer(function(req, res){
 	fs.readFile('demoFile.html', function(err, data){ // read files -> html files
 	
 		res.writeHead(200, {'Content-Type': 'text/html'});// return a response to the client , 200 -> all is OK, 2argument-> response headers
-		
 		res.write("A data e o horario hoje: " + dt.myDateTime()); // module function
 		
+		res.write(req.url);
 		var q = url.parse(req.url, true).query;
 		var txt = q.year + " " + q.month;
 		
-		res.write(req.url);
+		
 		
 		res.end(txt);
 		res.end('Teste Server Node JS '); //end the response
+		
 	});
 }).listen(8080); // listen port 8080
+
+console.log(q.host); // returns 'localhost:8080'
+console.log(q.pathname); // returns '/default.htm'
+console.log(q.search); // returns '?year=2019&month=september'
+
+var qdata = q.query; // returns an object: { year: 2019, month: 'september'}
+console.log(qdata.month); // returns 'september'
