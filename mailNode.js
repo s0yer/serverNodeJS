5adff49eb13f7bf4591c5a4af9d9ee23eb835a4c
jadsonmarliere <jds.mdo@gmail.com>
2019-09-15 12:44:17 -0300

    Update mailNode.js

--- mailNode.js
+++ mailNode.js
@@ -1,9 +1,24 @@
-var emailnode = require('nodemailer');
+var emailnode = require('nodemailer'); // modulo para enviar o email
 
 var transporte = emailnode.createTransport({
 	service: 'gmail',
 	auth: {
-		user:
-		pass:
+		user:'myemail@gmail.com'
+		pass:'mypass'
+	}
+});
+
+var emailDescricao = {
+	from:'myemail@gmail.com'
+	to:'destino@hotmail.com'
+	subject:'Teste email nodeJS'
+	text:'EasyPeasy'
+};
+
+transporte.sendEmail(emailDescricao, function(error, info){
+	if(error){
+		console.log(Falha no envio);
+	}else{
+			console.log('Mensagem eletrônica enviada: ' + info.response);
 	}
 });
